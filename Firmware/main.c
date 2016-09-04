64e25f3a3bd0c9869d384db269cbe631c39eb3ee
Bogdan Kalinin <bogdan.kalinin@gmail.com>
2016-09-04 17:25:32 +0300

    Update: main.c. 0 level fixed. To fix: 255 level.

---
+++ Firmware/main.c
@@ -0,0 +1,174 @@
+#define COLORS      3
+#define CHANELS     6
+
+#define RED         0
+#define GREEN       1
+#define BLUE        2
+
+/*@{*/ // Ports setup
+#define DDR_RED0    DDRD
+#define DDR_GREEN0  DDRD
+#define DDR_BLUE0   DDRD
+#define DDR_RED1    DDRC
+#define DDR_GREEN1  DDRC
+#define DDR_BLUE1   DDRC
+#define DDR_RED2    DDRB
+#define DDR_GREEN2  DDRB
+#define DDR_BLUE2   DDRB
+#define DDR_RED3    DDRB
+#define DDR_GREEN3  DDRB
+#define DDR_BLUE3   DDRB
+#define DDR_RED4    DDRB
+#define DDR_GREEN4  DDRB
+#define DDR_BLUE4   DDRB
+#define DDR_RED5    DDRB
+#define DDR_GREEN5  DDRB
+#define DDR_BLUE5   DDRB
+
+#define PORT_RED0   PORTD
+#define PORT_GREEN0 PORTD
+#define PORT_BLUE0  PORTD
+#define PORT_RED1   PORTC
+#define PORT_GREEN1 PORTC
+#define PORT_BLUE1  PORTC
+#define PORT_RED2   PORTB
+#define PORT_GREEN2 PORTB
+#define PORT_BLUE2  PORTB
+#define PORT_RED3   PORTB
+#define PORT_GREEN3 PORTB
+#define PORT_BLUE3  PORTB
+#define PORT_RED4   PORTB
+#define PORT_GREEN4 PORTB
+#define PORT_BLUE4  PORTB
+#define PORT_RED5   PORTB
+#define PORT_GREEN5 PORTB
+#define PORT_BLUE5  PORTB
+
+#define RED0        PD2
+#define GREEN0      PD3
+#define BLUE0       PD4
+#define RED1        PC5
+#define GREEN1      PC4
+#define BLUE1       PC3
+#define RED2        PB5
+#define GREEN2      PB5
+#define BLUE2       PB5
+#define RED3        PB5
+#define GREEN3      PB5
+#define BLUE3       PB5
+#define RED4        PB5
+#define GREEN4      PB5
+#define BLUE4       PB5
+#define RED5        PB5
+#define GREEN5      PB5
+#define BLUE5       PB5
+/*@}*/
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+uint8_t leds[COLORS][CHANELS];              // Values RGB LEDs. User change
+uint8_t leds_buff[COLORS][CHANELS];         // Protected buffer RGB values. Program use only
+uint8_t countPWM = 0;                       // Counter for software PWM
+
+inline void InitPorts() {
+    DDR_RED0 |= 1<<RED0;
+    DDR_RED1 |= 1<<RED1;
+    DDR_RED2 |= 1<<RED2;
+    DDR_RED3 |= 1<<RED3;
+    DDR_RED4 |= 1<<RED4;
+    DDR_RED5 |= 1<<RED5;
+    DDR_GREEN0 |= 1<<GREEN0;
+    DDR_GREEN1 |= 1<<GREEN1;
+    DDR_GREEN2 |= 1<<GREEN2;
+    DDR_GREEN3 |= 1<<GREEN3;
+    DDR_GREEN4 |= 1<<GREEN4;
+    DDR_GREEN5 |= 1<<GREEN5;
+    DDR_BLUE0 |= 1<<BLUE0;
+    DDR_BLUE1 |= 1<<BLUE1;
+    DDR_BLUE2 |= 1<<BLUE2;
+    DDR_BLUE3 |= 1<<BLUE3;
+    DDR_BLUE4 |= 1<<BLUE4;
+    DDR_BLUE5 |= 1<<BLUE5;
+}
+
+void Init() {
+    InitPorts();
+    TCCR0B |= 1<<CS00;                      // 1:1 F_T0
+    TIMSK0 |= 1<<TOIE0;
+}
+
+int main() {
+    Init();
+    sei();
+
+    leds[RED][0] = 1;
+    leds[GREEN][0] = 0;
+    leds[BLUE][0] = 0;
+
+    // Off inverted chanels
+    leds[RED][2] = 255;
+    leds[GREEN][2] = 255;
+    leds[BLUE][2] = 255;
+    leds[RED][3] = 255;
+    leds[GREEN][3] = 255;
+    leds[BLUE][3] = 255;
+    leds[RED][4] = 255;
+    leds[GREEN][4] = 255;
+    leds[BLUE][4] = 255;
+    leds[RED][5] = 255;
+    leds[GREEN][5] = 255;
+    leds[BLUE][5] = 255;
+
+    while(1){
+        _delay_ms(10);
+    }
+}
+
+ISR (TIMER0_OVF_vect) {
+    if (!countPWM) {
+        for (uint8_t i=0; i<COLORS; i++) {
+            for (uint8_t j=0; j<CHANELS; j++) {
+                leds_buff[i][j] = leds[i][j];
+            }
+        }
+        if (countPWM != leds_buff[RED][0])   PORT_RED0   &= ~(1<<RED0);
+        if (countPWM != leds_buff[RED][1])   PORT_RED1   &= ~(1<<RED1);
+        if (countPWM != leds_buff[RED][2])   PORT_RED2   &= ~(1<<RED2);
+        if (countPWM != leds_buff[RED][3])   PORT_RED3   &= ~(1<<RED3);
+        if (countPWM != leds_buff[RED][4])   PORT_RED4   &= ~(1<<RED4);
+        if (countPWM != leds_buff[RED][5])   PORT_RED5   &= ~(1<<RED5);
+        if (countPWM != leds_buff[GREEN][0]) PORT_GREEN0 &= ~(1<<GREEN0);
+        if (countPWM != leds_buff[GREEN][1]) PORT_GREEN1 &= ~(1<<GREEN1);
+        if (countPWM != leds_buff[GREEN][2]) PORT_GREEN2 &= ~(1<<GREEN2);
+        if (countPWM != leds_buff[GREEN][3]) PORT_GREEN3 &= ~(1<<GREEN3);
+        if (countPWM != leds_buff[GREEN][4]) PORT_GREEN4 &= ~(1<<GREEN4);
+        if (countPWM != leds_buff[GREEN][5]) PORT_GREEN5 &= ~(1<<GREEN5);
+        if (countPWM != leds_buff[BLUE][0])  PORT_BLUE0  &= ~(1<<BLUE0);
+        if (countPWM != leds_buff[BLUE][1])  PORT_BLUE1  &= ~(1<<BLUE1);
+        if (countPWM != leds_buff[BLUE][2])  PORT_BLUE2  &= ~(1<<BLUE2);
+        if (countPWM != leds_buff[BLUE][3])  PORT_BLUE3  &= ~(1<<BLUE3);
+        if (countPWM != leds_buff[BLUE][4])  PORT_BLUE4  &= ~(1<<BLUE4);
+        if (countPWM != leds_buff[BLUE][5])  PORT_BLUE5  &= ~(1<<BLUE5);
+    }
+    if (countPWM == leds_buff[RED][0])   PORT_RED0   |= 1<<RED0;
+    if (countPWM == leds_buff[RED][1])   PORT_RED1   |= 1<<RED1;
+    if (countPWM == leds_buff[RED][2])   PORT_RED2   |= 1<<RED2;
+    if (countPWM == leds_buff[RED][3])   PORT_RED3   |= 1<<RED3;
+    if (countPWM == leds_buff[RED][4])   PORT_RED4   |= 1<<RED4;
+    if (countPWM == leds_buff[RED][5])   PORT_RED5   |= 1<<RED5;
+    if (countPWM == leds_buff[GREEN][0]) PORT_GREEN0 |= 1<<GREEN0;
+    if (countPWM == leds_buff[GREEN][1]) PORT_GREEN1 |= 1<<GREEN1;
+    if (countPWM == leds_buff[GREEN][2]) PORT_GREEN2 |= 1<<GREEN2;
+    if (countPWM == leds_buff[GREEN][3]) PORT_GREEN3 |= 1<<GREEN3;
+    if (countPWM == leds_buff[GREEN][4]) PORT_GREEN4 |= 1<<GREEN4;
+    if (countPWM == leds_buff[GREEN][5]) PORT_GREEN5 |= 1<<GREEN5;
+    if (countPWM == leds_buff[BLUE][0])  PORT_BLUE0  |= 1<<BLUE0;
+    if (countPWM == leds_buff[BLUE][1])  PORT_BLUE1  |= 1<<BLUE1;
+    if (countPWM == leds_buff[BLUE][2])  PORT_BLUE2  |= 1<<BLUE2;
+    if (countPWM == leds_buff[BLUE][3])  PORT_BLUE3  |= 1<<BLUE3;
+    if (countPWM == leds_buff[BLUE][4])  PORT_BLUE4  |= 1<<BLUE4;
+    if (countPWM == leds_buff[BLUE][5])  PORT_BLUE5  |= 1<<BLUE5;
+    countPWM++;
+}
---
+++ Firmware/test.c
@@ -0,0 +1,127 @@
+#define COLORS      3
+#define CHANELS     6
+
+#define RED         0
+#define GREEN       1
+#define BLUE        2
+
+#define DDR_RED0    DDRD
+#define DDR_GREEN0  DDRD
+#define DDR_BLUE0   DDRD
+#define DDR_RED1    DDRC
+#define DDR_GREEN1  DDRC
+#define DDR_BLUE1   DDRC
+#define PORT_RED0   PORTD
+#define PORT_GREEN0 PORTD
+#define PORT_BLUE0  PORTD
+#define PORT_RED1   PORTC
+#define PORT_GREEN1 PORTC
+#define PORT_BLUE1  PORTC
+#define RED0        PD2
+#define GREEN0      PD3
+#define BLUE0       PD4
+#define RED1        PC5
+#define GREEN1      PC4
+#define BLUE1       PC3
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+uint8_t leds[COLORS][CHANELS];
+uint8_t leds_buff[COLORS][CHANELS];
+uint8_t counter = 0;
+
+int main() {
+    DDR_RED0   |= 1<<RED0;
+    DDR_GREEN0 |= 1<<GREEN0;
+    DDR_BLUE0  |= 1<<BLUE0;
+    DDR_RED1   |= 1<<RED1;
+    DDR_GREEN1 |= 1<<GREEN1;
+    DDR_BLUE1  |= 1<<BLUE1;
+    TCCR0B |= 1<<CS00;
+    TIMSK0 |= 1<<TOIE0;
+    sei();
+
+    for (uint8_t i=0; i<COLORS; i++) {
+        for (uint8_t j=0; j<CHANELS; j++) {
+            leds[i][j] = 0;
+            leds_buff[i][j] = 0;
+        }
+    }
+    uint8_t temp = 1;
+    while(1) {
+        if (temp == 1) {
+            if (leds[GREEN][0] < 255) {
+                leds[GREEN][0]++;
+                leds[RED][1] = leds[GREEN][0];
+            }
+            else
+                temp = 2;
+        }
+        if (temp == 2) {
+            if (leds[RED][0] > 0) {
+                leds[RED][0]--;
+                leds[RED][1] = leds[RED][0];
+            }
+            else
+                temp = 3;
+        }
+        if (temp == 3) {
+            if (leds[BLUE][0] < 255) {
+                leds[BLUE][0]++;
+                leds[GREEN][1] = leds[BLUE][0];
+            }
+            else
+                temp = 4;
+        }
+        if (temp == 4) {
+            if (leds[GREEN][0] > 0) {
+                leds[GREEN][0]--;
+                leds[GREEN][1] = leds[GREEN][0];
+            }
+            else
+                temp = 5;
+        }
+        if (temp == 5) {
+            if (leds[RED][0] < 255) {
+                leds[RED][0]++;
+                leds[BLUE][1] = leds[RED][0];
+            }
+            else
+                temp = 6;
+        }
+        if (temp == 6) {
+            if (leds[BLUE][0] > 0) {
+                leds[BLUE][0]--;
+                leds[BLUE][1] = leds[BLUE][0];
+            }
+            else
+                temp = 1;
+        }
+        _delay_ms(50);
+    }
+}
+
+ISR (TIMER0_OVF_vect) {
+    if (counter == 0) {
+        for (uint8_t i=0; i<COLORS; i++) {
+            for (uint8_t j=0; j<CHANELS; j++) {
+                leds_buff[i][j] = leds[i][j];
+            }
+        }
+        PORT_RED0   &= ~(1<<RED0);
+        PORT_RED1   &= ~(1<<RED1);
+        PORT_GREEN0 &= ~(1<<GREEN0);
+        PORT_GREEN1 &= ~(1<<GREEN1);
+        PORT_BLUE0  &= ~(1<<BLUE0);
+        PORT_BLUE1  &= ~(1<<BLUE1);
+    }
+    counter++;
+    if (counter == leds_buff[RED][0])   PORT_RED0   |= 1<<RED0;
+    if (counter == leds_buff[RED][1])   PORT_RED1   |= 1<<RED1;
+    if (counter == leds_buff[GREEN][0]) PORT_GREEN0 |= 1<<GREEN0;
+    if (counter == leds_buff[GREEN][1]) PORT_GREEN1 |= 1<<GREEN1;
+    if (counter == leds_buff[BLUE][0])  PORT_BLUE0  |= 1<<BLUE0;
+    if (counter == leds_buff[BLUE][1])  PORT_BLUE1  |= 1<<BLUE1;
+}
