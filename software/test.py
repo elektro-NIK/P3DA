e662a47257adeb6a608b362d9177069e74eaa58e
Bohdan Kalinin <bogdan.kalinin@gmail.com>
2017-02-06 14:23:10 +0200

    Add gamma correction

--- software/test.py
+++ software/test.py
@@ -2,29 +2,32 @@
 import serial
 from time import sleep
 
+
+def gamma(x): return round((x / 255) ** 2.8 * 511)
+
+
 con = serial.Serial('/dev/ttyUSB0', 38400, timeout=0.1)
 mode = 0
 r = 0x1ff
 g = 0x000
 b = 0x000
+
 while True:
-    if mode == 0:
-        if g < 0x1ff: g += 1
-        else: mode = 1
-    elif mode == 1:
-        if r > 0x000: r -= 1
-        else: mode = 2
-    elif mode == 2:
-        if b < 0x1ff: b += 1
-        else: mode = 3
-    elif mode == 3:
-        if g > 0x000: g -= 1
-        else: mode = 4
-    elif mode == 4:
-        if r < 0x1ff: r += 1
-        else: mode = 5
-    elif mode == 5:
-        if b > 0x00: b -= 1
-        else: mode = 0
-    con.write(('#S0' + '{:03x}{:03x}{:03x}'.format(r, g, b)).encode())
+    if mode == 0 and g < 0xff:
+        g += 1
+    elif mode == 1 and r > 0x00:
+        r -= 1
+    elif mode == 2 and b < 0xff:
+        b += 1
+    elif mode == 3 and g > 0x00:
+        g -= 1
+    elif mode == 4 and r < 0xff:
+        r += 1
+    elif mode == 5 and b > 0x00:
+        b -= 1
+    elif mode != 5:
+        mode += 1
+    else:
+        mode = 0
+    con.write(('#S0' + '{:03x}{:03x}{:03x}'.format(gamma(r), gamma(g), gamma(b))).encode())
     sleep(0.0005)
